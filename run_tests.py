86359808d0d16b8a2e609a65e5a7b0b184079722
brent hartshorn <brenthartshorn@proton.me>
2026-08-04 22:28:34 -0700

    run flavor test

--- run_tests.py
+++ run_tests.py
@@ -15,7 +15,8 @@ SUITES = ["tests/test_viz.py", "tests/test_pycicy.py",
           "tests/test_knots.py", "tests/test_chirality.py",
           "tests/test_hyperbolic.py", "tests/test_apolynomial.py", "tests/test_bundles.py",
           "tests/test_hofstadter.py",
-          "tests/test_polytope.py"]
+          "tests/test_polytope.py",
+          "tests/test_flavor.py"]
 
 
 def main():
